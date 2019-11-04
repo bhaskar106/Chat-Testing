0121d7afae4b94885a34bba42497be984cb88a19
bhaskar106 <54832857+bhaskar106@users.noreply.github.com>
2019-11-04 15:41:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,33 +1,51 @@
-var http = require('http')
-var fs = require('fs')
-var io = require('socket.io')(app)
-var app = http.createServer(function(req,res){
-  fs.readFile('Chat.html',function(err,data){
-    res.writeHead(200,{'Content-Type': 'text/html'})
-    res.write(data)
-    res.end()
-  })
+var app =require('express')();
+
+var http = require('http').Server(app);
+
+var io = require ('socket.io')(http);
+
+app.get('/', (req, res)  => {
+  res.sendFile(__dirname + '/client.html');
 })
-app.listen(106)
 
-var users = []
+http.listen(106, () =>{
+  console.log('Server is started')
+})
 
-io.on('connection', socket => {
-  socket.on('new-user', function (name , callback) {
-    if(name.indexOf(name)!=-1){
-      callback(false);
-    }
-    else{
-      socket.users=name
-      users.push(socket.users)
-      io.sockets.emit('user-connected',users)
-    }
+var users = {};
+var clients = [];
+
+io.sockets.on('connection', socket => {
+  console.log('connected');
+  socket.on('new-user', name => {
+    console.log('new user');
+      //users[socket.id] = name;
+      users[socket.id] = name;
+      //clients[id]=users;
+      io.sockets.emit('username',name);
+      socket.emit('id',socket.id);
+      socket.emit('Allusers',users);
+      console.log(Object.keys(users));
+      console.log(Object.values(users));
+      clients = Object.keys(users);
+      socket.emit('clients', Object.values(users));
+      socket.broadcast.emit('user-connected', name);
   })
+
+
   socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
+    console.log('sending message');
+    socket.broadcast.emit('chat-message', { message:message, name: users[socket.id] });
+   socket.broadcast.to(users[socket.id]).emit('message',{ message:message, name: users[socket.id] });
+  });
+
+
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+    console.log('disconnected');
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+    //users.splice(users.indexOf(socket), 1);
+  });
+
+
+});
