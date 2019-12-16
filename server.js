5f3f0476626f5cf6ee22c8ad3ee947ed309b2e47
bhaskar106 <54832857+bhaskar106@users.noreply.github.com>
2019-12-16 19:36:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -31,38 +31,37 @@ io.sockets.on('connection', socket => {
 
 
    socket.on('new-user', name => {
-      console.log('new user');
       io.sockets.emit('name',name);
       users[socket.id] = name;
       var Value = [
-          [socket.id,name]
+          [0,name]
        ];
-      connection.query("INSERT INTO Clients(SocketId,UserName) VALUES ?", [Value] , function (err, result, fields) {
+      connection.query("INSERT INTO Clients(Id,UserName) VALUES ?", [Value] , function (err, result, fields) {
         if (err) throw err;
        console.log(result);
        });
       socket.emit('id',socket.id);
       socket.emit('Allusers',users);
-      console.log(Object.keys(users));
-      console.log(Object.values(users));
-      console.log(users);
+      //console.log(Object.keys(users));
+      //console.log(Object.values(users));
+      //console.log(users);
       clients=Object(users);
       socket.emit('clients', Object.values(users));
       socket.broadcast.emit('user-connected', name);
       var U=(Object.values(users))
       var u=(Object.keys(users))
-      console.log(U.length)
-      console.log(u.length-1)
-      console.log(socket.id)
+      //console.log(U.length)
+      //console.log(u.length-1)
+      //console.log(socket.id)
       for(i=0;i<U.length-1;i++)
       {
-      console.log("for loop");
-      console.log(U.length)
+      //console.log("for loop");
+      //console.log(U.length)
       if(U[U.length-1] == U[i])
       {
         io.to(socket.id).emit('Userdisconnect',{name:`${name}`});
-        console.log("if condition");
-        console.log(u[U.length-1]);
+        //console.log("if condition");
+        //console.log(u[U.length-1]);
         break;
       }
       }
@@ -74,34 +73,51 @@ io.sockets.on('connection', socket => {
     var N=`${Value.Select}`;
     var M=`${Value.message}`;
     a = new Date();
+    console.log(a);
     d = a.toJSON();
+    console.log(d);
     date = d.substring(0,10);
+    console.log(date);
     time = d.substring(11,19);
+    console.log(time);
     var name= users[socket.id];
-    var Value = [
-          [name,d,M,N]
+    Value = [
+          [name,date,time,M,N]
        ];
-      connection.query("INSERT INTO Mdata(UserName,Date,Message,ToUser) VALUES ?", [Value] , function (err, result, fields) {
+      connection.query("INSERT INTO Mdata(UserName,Date,Time,Message,ToUser) VALUES ?", [Value] , function (err, result, fields) {
         if (err) throw err;
        console.log(result);
        });
 
-    console.log(N);
-    console.log(V.indexOf(N));
-    console.log(K[V.indexOf(N)]);
-    console.log('entered option value')
-    console.log(M); 
+    //console.log(N);
+    //console.log(V.indexOf(N));
+    //console.log(K[V.indexOf(N)]);
+    //console.log('entered option value')
+    //console.log(M); 
      if(V.indexOf(N) != -1)
      {
-      console.log("entered if condition")
+      //console.log("entered if condition")
       socket.to(K[V.indexOf(N)]).emit('chat-message',{ message: M, name: users[socket.id] });
-      console.log("closing if")
+      //console.log("closing if")
+      Value = [users[socket.id]];
+      connection.query("SELECT * FROM Mdata WHERE UserName= ?",[Value] , function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        });
+      connection.query("SELECT * FROM Mdata WHERE ToUser= ?",[Value] , function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        });
      }
      else
      {
-      console.log("enter else")
+      //console.log("enter else")
       socket.broadcast.emit('chat-message', { message: M, name: users[socket.id] });
-      console.log("close else")
+      //console.log("close else")
+      connection.query("SELECT * FROM Mdata", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        });
      }
   });
 
